5f5d92bc417939e2d5d532822b1a9030bd9072dd
Mariam <mariamaouladomar80@gmail.com>
2026-06-12 13:07:45 +0200

    Esta sí :)

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -31,7 +31,7 @@ const nextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'ktwnpnzopnucgfpmleuk.supabase.co',
+        hostname: 'bxpxnfbddesaduvwvaem.supabase.co',
       },
     ],
   },
